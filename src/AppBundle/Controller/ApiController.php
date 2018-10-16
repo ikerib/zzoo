6a4c4b5d2734262f24a9846d324f202290af69eb
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2018-10-16 14:51:36 +0200

    Ez dakit zer aldaketak diren, itxura dirudite

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -29,281 +29,291 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class ApiController extends FOSRestController
 {
 
-    /**
-     * @Route("/")
-     */
-    public function indexAction ( Request $request )
-    {
-        return $this->redirectToRoute('nelmio_api_doc_index', array(), 301);
-    }
+  /**
+   * @Route("/")
+   */
+  public function indexAction(Request $request)
+  {
+    return $this->redirectToRoute('nelmio_api_doc_index', array(), 301);
+  }
 
 
 //    ORDENANTZAK
 
-    /**
-     * Udal baten Ordenantza zerrenda Udal-Kodea bidez.
-     *
-     * @ApiDoc(
-     *   resource = true,
-     *   description = "Ordenantza guztien zerrenda eskuratu",
-     *   statusCodes = {
-     *     200 = "Zuzena denean"
-     *   }
-     * )
-     *
-     *
-     * @return array|View
-     * @Annotations\View()
-     * @Get("/ordenantzakbykodea/{kodea}.{_format}")
-     */
-    public function getOrdenantzakbykodeaAction($kodea)
-    {
-        $em = $this->getDoctrine()->getManager();
-        /** @var  $query \Doctrine\DBAL\Query\QueryBuilder */
-        $query = $em->createQuery( /** @lang text */
-            '
+  /**
+   * Udal baten Ordenantza zerrenda Udal-Kodea bidez.
+   *
+   * @ApiDoc(
+   *   resource = true,
+   *   description = "Ordenantza guztien zerrenda eskuratu",
+   *   statusCodes = {
+   *     200 = "Zuzena denean"
+   *   }
+   * )
+   *
+   *
+   * @return array|View
+   * @Annotations\View()
+   * @Get("/ordenantzakbykodea/{kodea}.{_format}")
+   */
+  public function getOrdenantzakbykodeaAction($kodea)
+  {
+    $em = $this->getDoctrine()->getManager();
+    /** @var  $query \Doctrine\DBAL\Query\QueryBuilder */
+    $query = $em->createQuery(
+    /** @lang text */
+      '
             SELECT o 
             FROM AppBundle:Ordenantza o
                INNER JOIN o.udala u
             WHERE u.kodea = :udalkodea AND ((o.ezabatu IS NULL) or (o.ezabatu <> 1))
-            ');
-        $query->setParameter( 'udalkodea', $kodea );
-        $ordenantzak = $query->getResult();
-        $view = View::create();
-        $view->setData( $ordenantzak );
-        header( 'content-type: application/json; charset=utf-8' );
-        header( "access-control-allow-origin: *" );
+            '
+    );
+    $query->setParameter('udalkodea', $kodea);
+    $ordenantzak = $query->getResult();
+    $view        = View::create();
+    $view->setData($ordenantzak);
+    header('content-type: application/json; charset=utf-8');
+    header("access-control-allow-origin: *");
 
-        return $view;
-    }
-
-
-    /**
-     * Udal baten Ordenantza zerrenda.
-     *
-     * @ApiDoc(
-     *   resource = true,
-     *   description = "Ordenantza guztien zerrenda eskuratu",
-     *   statusCodes = {
-     *     200 = "Zuzena denean"
-     *   }
-     * )
-     *
-     *
-     * @return array|View
-     *
-     * @Annotations\View()
-     * @Get("/ordenantzakbyid/{udalaid}")
-     */
-    public function getOrdenantzakAction($udalaid)
-    {
-
-        $em = $this->getDoctrine()->getManager();
-        $ordenantzak = $em->getRepository( 'AppBundle:Ordenantza' )->findBy(
-            array (
-                'udala.kodea' => $udalaid,
-            )
-        );
-
-        $view = View::create();
-        $view->setData( $ordenantzak );
-        header( 'content-type: application/json; charset=utf-8' );
-        header( "access-control-allow-origin: *" );
-
-        return $view;
-    }
+    return $view;
+  }
 
 
+  /**
+   * Udal baten Ordenantza zerrenda.
+   *
+   * @ApiDoc(
+   *   resource = true,
+   *   description = "Ordenantza guztien zerrenda eskuratu",
+   *   statusCodes = {
+   *     200 = "Zuzena denean"
+   *   }
+   * )
+   *
+   *
+   * @return array|View
+   *
+   * @Annotations\View()
+   * @Get("/ordenantzakbyid/{udalaid}")
+   */
+  public function getOrdenantzakAction($udalaid)
+  {
+
+    $em          = $this->getDoctrine()->getManager();
+    $ordenantzak = $em->getRepository('AppBundle:Ordenantza')->findBy(
+      array(
+        'udala.kodea' => $udalaid,
+      )
+    );
+
+    $view = View::create();
+    $view->setData($ordenantzak);
+    header('content-type: application/json; charset=utf-8');
+    header("access-control-allow-origin: *");
+
+    return $view;
+  }
 
 
+  /**
+   * @ApiDoc(
+   *   resource = true,
+   *   description = "Ordenantza baten informazioa eskuratu"
+   * )
+   *
+   * @Annotations\View()
+   * @Get("/ordenantza/{id}")
+   */
+  public function getOrdenantzaAction($id)
+  {
+    $em         = $this->getDoctrine()->getManager();
+    $ordenantza = $em->getRepository('AppBundle:Ordenantza')->findById($id);
+    header('content-type: application/json; charset=utf-8');
+    header("access-control-allow-origin: *");
 
-
-    /**
-     * @ApiDoc(
-     *   resource = true,
-     *   description = "Ordenantza baten informazioa eskuratu"
-     * )
-     *
-     * @Annotations\View()
-     * @Get("/ordenantza/{id}")
-     */
-    public function getOrdenantzaAction($id){
-        $em         = $this->getDoctrine()->getManager();
-        $ordenantza = $em->getRepository('AppBundle:Ordenantza')->findById($id);
-        header('content-type: application/json; charset=utf-8');
-        header("access-control-allow-origin: *");
-        return $ordenantza;
-    }
+    return $ordenantza;
+  }
 
 
 //    ATALAK
 
-    /**
-     * Ordenantza batem tributu guztien zerrenda.
-     *
-     * @ApiDoc(
-     *   resource = true,
-     *   description = "Ordenantza baten tributu guztien zerrenda eskuratu",
-     *   statusCodes = {
-     *     200 = "Zuzena denean"
-     *   }
-     * )
-     *
-     *
-     * @param $ordenantzaid
-     *
-     * @return View
-     * @Annotations\View()
-     * @Get("/tributuak/{ordenantzaid}")
-     */
-    public function getAtalakAction($ordenantzaid)
-    {
-        $em         = $this->getDoctrine()->getManager();
+  /**
+   * Ordenantza batem tributu guztien zerrenda.
+   *
+   * @ApiDoc(
+   *   resource = true,
+   *   description = "Ordenantza baten tributu guztien zerrenda eskuratu",
+   *   statusCodes = {
+   *     200 = "Zuzena denean"
+   *   }
+   * )
+   *
+   *
+   * @param $ordenantzaid
+   *
+   * @return View
+   * @Annotations\View()
+   * @Get("/tributuak/{ordenantzaid}")
+   */
+  public function getAtalakAction($ordenantzaid)
+  {
+    $em = $this->getDoctrine()->getManager();
 //        $atalak = $em->getRepository('AppBundle:Atala')->findBy(array('ordenantza'=>$ordenantzaid));
-        /** @var  $query \Doctrine\DBAL\Query\QueryBuilder */
-        $query = $em->createQuery( /** @lang text */
-            '
+    /** @var  $query \Doctrine\DBAL\Query\QueryBuilder */
+    $query = $em->createQuery(
+    /** @lang text */
+      '
             SELECT a 
             FROM AppBundle:Atala a
                INNER JOIN a.ordenantza o
             WHERE o.id = :ordenantzaid AND ((a.ezabatu IS NULL) or (a.ezabatu <> 1))
-            ');
-        $query->setParameter( 'ordenantzaid', $ordenantzaid );
-        $atalak = $query->getResult();
+            '
+    );
+    $query->setParameter('ordenantzaid', $ordenantzaid);
+    $atalak = $query->getResult();
 
-        $view = View::create();
-        $view->setData($atalak);
-        header('content-type: application/json; charset=utf-8');
-        header("access-control-allow-origin: *");
-        return $view;
+    $view = View::create();
+    $view->setData($atalak);
+    header('content-type: application/json; charset=utf-8');
+    header("access-control-allow-origin: *");
 
-    }
+    return $view;
 
-    /**
-     * @ApiDoc(
-     *   resource = true,
-     *   description = "Tributu baten informazioa eskuratu"
-     * )
-     *
-     * @Annotations\View()
-     * @Get("/tributua/{id}")
-     */
-    public function getAtalaAction($id){
-        $em         = $this->getDoctrine()->getManager();
-        $atala = $em->getRepository('AppBundle:Atala')->findById($id);
-        header('content-type: application/json; charset=utf-8');
-        header("access-control-allow-origin: *");
-        return $atala;
-    }
+  }
+
+  /**
+   * @ApiDoc(
+   *   resource = true,
+   *   description = "Tributu baten informazioa eskuratu"
+   * )
+   *
+   * @Annotations\View()
+   * @Get("/tributua/{id}")
+   */
+  public function getAtalaAction($id)
+  {
+    $em    = $this->getDoctrine()->getManager();
+    $atala = $em->getRepository('AppBundle:Atala')->findById($id);
+    header('content-type: application/json; charset=utf-8');
+    header("access-control-allow-origin: *");
+
+    return $atala;
+  }
 
 
 //    AZPIATALAK
 
-    /**
-     * Udal baten zergen zerrenda.
-     *
-     * @ApiDoc(
-     *   resource = true,
-     *   description = "Udal baten zerga guztien zerrenda eskuratu",
-     *   statusCodes = {
-     *     200 = "Zuzena denean"
-     *   }
-     * )
-     *
-     *
-     * @return View
-     *
-     * @Annotations\View()
-     * @Get("/udalzergak/{udalaid}")
-     */
-    public function getAzpiatalakudalaAction($udalaid)
-    {
-        $em = $this->getDoctrine()->getManager();
-        /** @var  $query \Doctrine\DBAL\Query\QueryBuilder */
-        $query = $em->createQuery(
-            /** @lang text */
-            'SELECT p.id, p.kodea_prod, p.izenburuaeu_prod, p.izenburuaes_prod 
+  /**
+   * Udal baten zergen zerrenda.
+   *
+   * @ApiDoc(
+   *   resource = true,
+   *   description = "Udal baten zerga guztien zerrenda eskuratu",
+   *   statusCodes = {
+   *     200 = "Zuzena denean"
+   *   }
+   * )
+   *
+   *
+   * @return View
+   *
+   * @Annotations\View()
+   * @Get("/udalzergak/{udalaid}")
+   */
+  public function getAzpiatalakudalaAction($udalaid)
+  {
+    $em = $this->getDoctrine()->getManager();
+    /** @var  $query \Doctrine\DBAL\Query\QueryBuilder */
+    $query = $em->createQuery(
+    /** @lang text */
+      'SELECT p.id, p.kodea_prod, p.izenburuaeu_prod, p.izenburuaes_prod 
                 FROM AppBundle:Azpiatala p 
                 WHERE p.udala=:udalaid AND ((p.ezabatu IS NULL) or (p.ezabatu <> 1))
                 '
-        );
-        $query->setParameter( 'udalaid', $udalaid );
-        $azpiatalak = $query->getResult();
+    );
+    $query->setParameter('udalaid', $udalaid);
+    $azpiatalak = $query->getResult();
 
-        $view = View::create();
-        $view->setData($azpiatalak);
-        header('content-type: application/json; charset=utf-8');
-        header("access-control-allow-origin: *");
-        return $view;
+    $view = View::create();
+    $view->setData($azpiatalak);
+    header('content-type: application/json; charset=utf-8');
+    header("access-control-allow-origin: *");
 
-    }
+    return $view;
 
-    /**
-     * Udal baten zergen zerrenda.
-     *
-     * @ApiDoc(
-     *   resource = true,
-     *   description = "Udal baten zerga guztien zerrenda eskuratu",
-     *   statusCodes = {
-     *     200 = "Zuzena denean"
-     *   }
-     * )
-     *
-     *
-     * @param $tributuaid
-     *
-     * @return View
-     * @Annotations\View()
-     * @Get("/zergak/{tributuaid}")
-     */
-    public function getAzpiatalakAction($tributuaid)
-    {
-        $em = $this->getDoctrine()->getManager();
-        /** @var  $query \Doctrine\DBAL\Query\QueryBuilder */
-        $query = $em->createQuery( /** @lang text */
-            '
+  }
+
+  /**
+   * Udal baten zergen zerrenda.
+   *
+   * @ApiDoc(
+   *   resource = true,
+   *   description = "Udal baten zerga guztien zerrenda eskuratu",
+   *   statusCodes = {
+   *     200 = "Zuzena denean"
+   *   }
+   * )
+   *
+   *
+   * @param $tributuaid
+   *
+   * @return View
+   * @Annotations\View()
+   * @Get("/zergak/{tributuaid}")
+   */
+  public function getAzpiatalakAction($tributuaid)
+  {
+    $em = $this->getDoctrine()->getManager();
+    /** @var  $query \Doctrine\DBAL\Query\QueryBuilder */
+    $query = $em->createQuery(
+    /** @lang text */
+      '
                 SELECT p.id, p.kodea_prod, p.izenburuaeu_prod,p.izenburuaes_prod  
                     FROM AppBundle:Azpiatala p 
                 WHERE p.atala=:atalaid AND ((p.ezabatu IS NULL) or (p.ezabatu <> 1))
-          ');
-        $query->setParameter( 'atalaid', $tributuaid );
-        $azpiatalak = $query->getResult();
+          '
+    );
+    $query->setParameter('atalaid', $tributuaid);
+    $azpiatalak = $query->getResult();
 
-        $view = View::create();
-        $view->setData($azpiatalak);
-        header('content-type: application/json; charset=utf-8');
-        header("access-control-allow-origin: *");
-        return $view;
+    $view = View::create();
+    $view->setData($azpiatalak);
+    header('content-type: application/json; charset=utf-8');
+    header("access-control-allow-origin: *");
 
-    }
+    return $view;
 
-    /**
-     *
-     * @ApiDoc(
-     *   resource = true,
-     *   description = "Zerga baten informazioa eskuratu",
-     *   statusCodes = {
-     *     200 = "Zuzena denean"
-     *   }
-     * )
-     *
-     *
-     * @return View
-     *
-     * @Annotations\View()
-     * @Get("/zerga/{id}")
-     */
-    public function getAzpiatalaAction($id)
-    {
-        $em         = $this->getDoctrine()->getManager();
-        $azpiatalak = $em->getRepository( 'AppBundle:Azpiatala' )->findOneById( $id );
+  }
 
-        $view = View::create();
-        $view->setData($azpiatalak);
-        header('content-type: application/json; charset=utf-8');
-        header("access-control-allow-origin: *");
-        return $view;
+  /**
+   *
+   * @ApiDoc(
+   *   resource = true,
+   *   description = "Zerga baten informazioa eskuratu",
+   *   statusCodes = {
+   *     200 = "Zuzena denean"
+   *   }
+   * )
+   *
+   *
+   * @return View
+   *
+   * @Annotations\View()
+   * @Get("/zerga/{id}")
+   */
+  public function getAzpiatalaAction($id)
+  {
+    $em         = $this->getDoctrine()->getManager();
+    $azpiatalak = $em->getRepository('AppBundle:Azpiatala')->findOneById($id);
 
-    }
+    $view = View::create();
+    $view->setData($azpiatalak);
+    header('content-type: application/json; charset=utf-8');
+    header("access-control-allow-origin: *");
+
+    return $view;
+
+  }
 
 }
